ef0cdcd1d7359823725775352e9131ec6cfa8f67
bewpage <b.jedlinski@bewpage.com>
2017-11-23 17:18:57 +0100

    init commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to React</h1>
+                <p>the Chat App</p>
+            </div>
+        );
+    }
+}
+
+export default App;
